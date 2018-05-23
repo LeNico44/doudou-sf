1c41bb2562da86aa2a938adf312ef66f26df2bd7
LeNico44 <nicolas.houisse@epsi.fr>
2018-05-23 17:41:07 +0200

    Premier formulaire Symfony !!!!

---
+++ src/Form/DoudouType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Doudou;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class DoudouType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('Couleur')
+            ->add('type')
+            ->add('lieuDecouverte')
+            ->add('photo')
+            ->add('lat')
+            ->add('lng')
+            ->add('dateDecouverte')
+            ->add('submit', SubmitType::class, [
+                "label" => "Envoyer !"
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Doudou::class,
+        ]);
+    }
+}
